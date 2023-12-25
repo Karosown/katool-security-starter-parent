2a2e04803f5578116b00efe8f3a7f24b92962927
ZonglinWu <mail@wzl1.top>
2023-12-25 18:38:39 +0800

    2023-12-25 fixed: demo优化

---
+++ katool-security-demo/katool-security-boot-simple-demo/src/main/java/cn/katool/security/demo/boot/simple/config/AuthConfig.java
@@ -0,0 +1,35 @@
+package cn.katool.security.demo.boot.simple.config;
+
+import cn.hutool.core.util.BooleanUtil;
+import cn.katool.security.core.logic.KaSecurityAuthLogic;
+import cn.katool.security.core.logic.KaToolSecurityAuthQueue;
+import cn.katool.security.core.model.entity.KaSecurityValidMessage;
+import cn.katool.security.starter.utils.KaSecurityAuthUtil;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+@Component
+public class AuthConfig extends KaSecurityAuthUtil<String> implements KaSecurityAuthLogic{
+    @Override
+    public KaSecurityValidMessage checkLogin(Boolean checkLogin) {
+        if (BooleanUtil.isFalse(checkLogin)){
+            return KaSecurityValidMessage.success();
+        }
+        return KaSecurityValidMessage.unLogin();
+    }
+
+    @Override
+    public KaSecurityValidMessage doAuth(List<String> roleList) {
+        // 这里可以根据角色列表进行鉴权，返回鉴权失败或者鉴权成功的消息
+        System.out.println("进入鉴权，roleList:" + roleList);
+        return KaSecurityValidMessage.success();
+    }
+
+    @Bean
+    private void initAuth(){
+        System.out.println("初始化鉴权框架");
+        KaToolSecurityAuthQueue.add(this);
+    }
+
+}
---
+++ katool-security-demo/katool-security-boot-simple-demo/src/main/java/cn/katool/security/demo/boot/simple/controller/TestController.java
@@ -0,0 +1,23 @@
+package cn.katool.security.demo.boot.simple.controller;
+
+import cn.katool.security.core.annotation.AuthCheck;
+import cn.katool.security.core.annotation.AuthControllerCheck;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@AuthControllerCheck(checkLogin = true)
+public class TestController {
+    @GetMapping
+    public String index() {
+        return "1";
+    }
+
+    @GetMapping("/needlogin")
+    public String needlogin() {
+        return "2";
+    }
+}
